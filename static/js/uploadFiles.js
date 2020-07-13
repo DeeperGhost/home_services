39d22546d021d0def2d3fae253e81724b18c70e5
KarlsonJuy <kareolis@gmail.com>
2020-07-13 12:54:53 +1000

    добавлены тестоваф модель данных

---
+++ static/js/uploadFiles.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+    //form Submit action
+    $("form#data").submit(function(event){
+        //disable the default form submission
+        event.preventDefault();
+        //grab all form data
+        var formData = new FormData($(this)[0]);
+
+        function setProgress(e) {
+            if (e.lengthComputable) {
+                var complete = e.loaded / e.total;
+                $("#pBar").text(Math.floor(complete*100)+"%");
+            }
+        }
+        $.ajax({
+            xhr: function() {
+                var xhr = new window.XMLHttpRequest();
+                xhr.upload.addEventListener("progress", setProgress, false);
+                xhr.addEventListener("progress", setProgress, false);
+                return xhr;
+            },
+//            url:"http://192.168.0.102:5000/test",
+            url: "/test",
+            type: 'POST',
+            data: formData,
+            async: true,
+            cache: false,
+            contentType: false,
+            processData: false,
+            enctype: 'multipart/form-data',
+           // success: function (returndata) {
+           //     alert(returndata);
+           // }
+        });
+        return false;
+    });
+});
